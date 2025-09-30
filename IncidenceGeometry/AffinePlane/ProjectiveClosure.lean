8b24a8d6a2319045cc4384d522ca4e9ae6bb7782
Paul <paul.schwahn@universite-paris-saclay.fr>
2025-09-30 14:56:44 -0300

    define join in proj.closure; use match syntax

--- IncidenceGeometry/AffinePlane/ProjectiveClosure.lean
+++ IncidenceGeometry/AffinePlane/ProjectiveClosure.lean
@@ -5,29 +5,31 @@ open IncidenceGeometry
 namespace AffinePlane
 
 inductive ProjectiveClosure.Point (P L : Type*) [AffinePlane P L] where
-  | ofAffine : P → Point P L
+  | point_of_affine : P → Point P L
   | point_at_infty : (π : Set L) → π ∈ Direction P L → Point P L
 
 inductive ProjectiveClosure.Line (P L : Type*) [AffinePlane P L] where
-  | ofAffine : L → Line P L
+  | line_of_affine : L → Line P L
   | line_at_infty : Line P L
+
+open ProjectiveClosure.Point
+open ProjectiveClosure.Line
 
 instance (P L : Type*) [AffinePlane P L] :
     IncidenceGeometry (ProjectiveClosure.Point P L) (ProjectiveClosure.Line P L) where
-  Incident := fun p l => by
-    cases p with
-    | ofAffine p =>
-      cases l with
-      | ofAffine l => exact p 𝐈 l
-      | line_at_infty => exact False
-    | point_at_infty π hπ =>
-      cases l with
-      | ofAffine l => exact l ∈ π
-      | line_at_infty => exact True
+  Incident := fun p l => match p, l with
+    | point_of_affine p', line_of_affine l' => p' 𝐈 l'
+    | point_of_affine _, line_at_infty => False
+    | point_at_infty π _, line_of_affine l' => l' ∈ π
+    | point_at_infty _ _, line_at_infty => True
 
-instance (P L : Type*) [AffinePlane P L] :
+noncomputable instance (P L : Type*) [AffinePlane P L] :
     ProjectivePlane (ProjectiveClosure.Point P L) (ProjectiveClosure.Line P L) where
-  join := sorry
+  join := fun p q ↦ match p, q with
+    | point_of_affine p', point_of_affine q' => line_of_affine (join p' q')
+    | point_of_affine p', point_at_infty π hπ => line_of_affine (directions_equiv_lines_through_a_point p' ⟨π, hπ⟩)
+    | point_at_infty π hπ, point_of_affine q' => line_of_affine (directions_equiv_lines_through_a_point q' ⟨π, hπ⟩)
+    | point_at_infty π₁ hπ₁, point_at_infty π₂ hπ₂ => line_at_infty
   join_incident := sorry
   unique_join := sorry
   meet := sorry
